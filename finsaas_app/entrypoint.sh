72c6cb8834d3f1a930c26084383723240e4d6b08
Pedro Brasil <ppbrasil@gmail.com>
2023-10-09 15:49:31 -0300

    docker-compose configured

--- finsaas_app/entrypoint.sh
+++ finsaas_app/entrypoint.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
+echo "Starting entrypoint.sh..."
+
 # wait for PSQL server to start
 sleep 10
 
 # Generate new migration files
 echo "Generating new migration files..."
-python manage.py makemigrations
+python3 manage.py makemigrations
 
 # Apply database migrations
 echo "Applying database migrations..."
-python manage.py migrate
+python3 manage.py migrate
 
 # collect static files
-python manage.py collectstatic --no-input
+python3 manage.py collectstatic --no-input
 
 # Start the Django development server
-# echo "Starting Django development server..."
-# python manage.py runserver 0.0.0.0:8000
+echo "Starting Django development server..."
+python3 manage.py runserver 0.0.0.0:8000
